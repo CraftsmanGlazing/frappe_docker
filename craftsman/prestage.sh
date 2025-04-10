3ca1bd0f41981bb1b37cda06068eb5cbb37cda47
CraftsmanGlazing <sysadmin@craftsmanglazing.com>
2025-04-10 16:56:04 -0700

    Update prestage.sh

--- craftsman/prestage.sh
+++ craftsman/prestage.sh
@@ -5,14 +5,16 @@ echo -n ${APPS_JSON_BASE64} | base64 -d
 
 printf 'Does this have all applications included (y/n)? '
 read answer
-if [ "$answer" != "${answer#[Yy]}" ] ;then 
+if [ "$answer" != "${answer#[Yy]}" ] ;then
     echo "Continuing...."
 else
     echo "Cancelled"
     exit
 fi
 echo "Building Local Docker Image this will take a wile.."
+
 cd ..
+
 sudo docker build \
   --build-arg=FRAPPE_PATH=https://github.com/frappe/frappe \
   --build-arg=FRAPPE_BRANCH=version-15 \
