d68d327f391552146050e1ece497e8d18be5a95c
CraftsmanGlazing <sysadmin@craftsmanglazing.com>
2025-04-17 12:45:55 -0700

    Update add_helpdesk.sh

--- craftsman/apps/add_helpdesk.sh
+++ craftsman/apps/add_helpdesk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Docker Info
-backend="docker_frappe_backend-1"
+backend="frappe_docker-backend-1"
 frontend="frappe_docker-frontend-1"
 scheduler="frappe_docker-scheduler-1"
 websocker="frappe_docker-websocket-1"
@@ -14,9 +14,9 @@ container_array=(${frontend} ${backend} ${scheduler} ${websocket} ${queueLong} $
 ##installing backend files in all containers
 for container in ${container_array[@]}; do
         echo $container
-        docker exec ${container} bench get https://github.com/frappe/helpdesk.git --branch version-14 --resolve-deps
+        docker exec ${container} bench get https://github.com/frappe/helpdesk.git --branch version-14 --resolve-deps &
 done
-
+wait
 #Installing App
 docker exec ${backend} bench --site frontend install-app helpdesk
 docker exec ${backend} bench --site frontend migrate
