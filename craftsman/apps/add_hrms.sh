15d79eec14860035b9452ee9805c179ed6531ef5
CraftsmanGlazing <sysadmin@craftsmanglazing.com>
2025-04-21 11:30:49 -0700

    Update add_hrms.sh

--- craftsman/apps/add_hrms.sh
+++ craftsman/apps/add_hrms.sh
@@ -14,7 +14,8 @@ container_array=(${frontend} ${backend} ${scheduler} ${websocket} ${queueLong} $
 ##installing backend files in all containers
 for container in ${container_array[@]}; do
         echo $container
-        docker exec ${container} bench get https://github.com/frappe/hrms.git --branch version-15 --resolve-deps &
+        #docker exec ${container} bench get https://github.com/frappe/hrms.git --branch version-15 --resolve-deps & ## --resolve-deps fails if you use custom ERP module
+        docker exec ${container} bench get https://github.com/frappe/hrms.git --branch version-15 &
 done
 wait
 #Installing App
