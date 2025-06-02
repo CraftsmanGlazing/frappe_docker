57a6cf1658189f40d425f1d80c2e650b1df5e04b
CraftsmanGlazing <sysadmin@craftsmanglazing.com>
2025-06-02 15:40:20 -0700

    Update production.sh
    Production Script

--- craftsman/production.sh
+++ craftsman/production.sh
@@ -1,3 +1,39 @@
-#!bin/bash
+#!/bin/bash
+#Install ERPNEXT Container
+
+cd ..
+docker compose -f pwd.yml up -d
+
+#Installing all Applications
+allcontainers=($(docker ps -a --format "{{.Names}}"))
+substring="frappe"
+containers=()
+
+for container in "${allcontainers[@]}"; do
+        if [[ "${container}" == *"$substring"* ]]; then
+                containers+=("${container}")
+        fi
+done
+
+substring="backend"
+
+for container in "${containers[@]}"; do
+    if [[ "$container" == *"$substring"* ]]; then
+        backend="$container"
+    fi
+done
+
+
+docker exec ${backend} bench --site frontend install-app print_designer
+docker exec ${backend} bench --site frontend install-app lms
+docker exec ${backend} bench --site frontend install-app hrms
+docker exec ${backend} bench --site frontend install-app helpdesk
+docker exec ${backend} bench --site frontend install-app crm
+
+docker exec ${backend} bench --site frontend migrate
+docker exec ${backend} bench --site frontend clear-cache
+
+
 # This has to be installed after
+cd craftsman
 docker compose -f https-compose.yml up -d
