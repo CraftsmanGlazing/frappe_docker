161a045c176d6b97a96627e297148a6c053ead87
CraftsmanGlazing <sysadmin@craftsmanglazing.com>
2025-06-10 20:55:46 -0700

    Update build-craftsman-site.sh

--- craftsman/build-craftsman-site.sh
+++ craftsman/build-craftsman-site.sh
@@ -1,1 +1,23 @@
+#!/bin/bash
 
+#Running The Docker Container Build
+sudo chmod + prestage.sh
+sudo ./prestage.sh
+
+localhostname=$(hostname)
+
+echo "What is the Domain?"
+read DOMAIN
+
+echo " Whats the Lookup in Domain?"
+read SUB
+
+cd ..
+docker compose -f pwd.yml up -d
+cd craftsman/
+
+export HTTP_HOSTNAME="$localhostname"
+export HTTP_DOMAIN="$DOMAIN"
+export HTTP_SUB="$SUB"
+
+docker compose -f https-compose.yml up -d
